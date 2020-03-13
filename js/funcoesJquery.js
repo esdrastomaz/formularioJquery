16ff61c47f7c8b8f2380b55a024f603dd42cd0a3
Esdras Tomaz <esdras_tomaz2@hotmail.com>
2020-03-13 19:49:07 -0300

    jquery

--- js/funcoesJquery.js
+++ js/funcoesJquery.js
@@ -2,3 +2,10 @@ function buscaDadosForm(){
     var txtNome = document.getElementById("nome").value;
     alert('O nome digitado é: ' + txtNome);
 }
+
+$(document).ready(function(){
+    $("#btnJquery").click(function(){
+        alert('O nome digitdado em jQuery é: ' + $("#nome").val());
+
+    });
+});
